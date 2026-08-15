9233d79397fc4309fa0babb4d7d1444f1b1bbdec
alanmichaelconceicaorodriguesm-ux <alanmichaelconceicaorodriguesm@gmail.com>
2026-08-15 10:42:06 -0300

    Create README.py

---
+++ README.py
@@ -0,0 +1,16 @@
+# meus-exercicios-python-iniciante - 5
+São questões simples para minha pessoa ter uma ser noção do meu progresso. 
+
+conj1 = {1, 2, 3, 4}
+conj2 = {3, 4, 5, 6}
+
+print("Primeiro conjunto: ", conj1)
+print("Segundo conjunto: ", conj2)
+
+uniao = conj1.union(conj2)
+intersecao = conj1.intersection(conj2)
+difer = conj1.difference(conj2)
+
+print("Resultado da união: ", uniao)
+print("Resultado da interseção: ", intersecao)
+print("Resultado da diferença: ", difer)
